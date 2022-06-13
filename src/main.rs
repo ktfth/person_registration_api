4e79b1a01f0d78c68e80296fd3fde4ac91ebb9a8
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-06-13 19:54:12 -0300

    feat: manually set headers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate rocket;
 
+use rocket::http::Header;
+use rocket::response::Responder;
 use rocket::serde::json::{Json};
 use rocket::serde::{Serialize};
 
@@ -24,6 +26,22 @@ struct Registration {
     data: String,
 }
 
+#[derive(Responder)]
+#[response(content_type = "application/json")]
+struct Register {
+    registration: Json<Registration>,
+    access_control_allow_origin: Header<'static>,
+}
+
+impl Register {
+    fn new(registration: Json<Registration>) -> Register {
+        Register {
+            registration,
+            access_control_allow_origin: Header::new("Access-Control-Allow-Origin", "*"),
+        }
+    }
+}
+
 #[get("/")]
 fn index() -> Json<Healthcheck<'static>> {
     Json(Healthcheck {
@@ -32,8 +50,8 @@ fn index() -> Json<Healthcheck<'static>> {
 }
 
 #[get("/?<kind>")]
-fn registration(kind: Kind) -> Json<Registration> {
-    match kind {
+fn registration(kind: Kind) -> Register {
+    let output = match kind {
         Kind::Physical => {
             Json(Registration {
                 data: Person::generate(),
@@ -49,10 +67,33 @@ fn registration(kind: Kind) -> Json<Registration> {
                 data: General::generate(),
             })
         }
-    }
+    };
+    Register::new(output)
+}
+
+#[options("/?<kind>")]
+fn registration_options(kind: Kind) -> Register {
+    let output = match kind {
+        Kind::Physical => {
+            Json(Registration {
+                data: Person::generate(),
+            })
+        },
+        Kind::Juridic => {
+            Json(Registration {
+                data: Juridic::generate(),
+            })
+        },
+        Kind::General => {
+            Json(Registration {
+                data: General::generate(),
+            })
+        }
+    };
+    Register::new(output)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, registration])
+    rocket::build().mount("/", routes![index, registration, registration_options])
 }
